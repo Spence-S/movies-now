19122a11a51411090bab6c27a94d20aaf70c4ee1
Spencer Snyder <sasnyde2@gmail.com>
2017-09-15 12:42:12 -0400

    initial commit

---
+++ controllers/movies.js
@@ -0,0 +1,43 @@
+const moviedb = require('moviedb')(process.env.MDB_API_KEY);
+
+exports.getMovies = (req, res) => {
+  let previousUrl;
+  let nextUrl;
+  let { page } = req.query;
+  if (typeof page === 'string') {
+    page = parseInt(page, 10);
+  }
+  if (!page || isNaN(page) || typeof page === 'undefined' || page <= 1) {
+    page = 1;
+    previousUrl = '/movies';
+    nextUrl = `/movies/?page=${page + 1}`;
+  } else {
+    previousUrl = `/movies/?page=${page - 1}`;
+    nextUrl = `/movies/?page=${page + 1}`;
+  }
+  // gives back paginated results for us
+  moviedb.miscNowPlayingMovies({ page }, (err, mdbres) => {
+    const totalPages = mdbres.total_pages;
+    const pagesArray = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pagesArray.push(i);
+    }
+    const options = {
+      mdb: mdbres,
+      page,
+      previousUrl,
+      nextUrl,
+      totalPages,
+      pagesArray,
+    };
+    res.render('movies/all-movies', options);
+  });
+};
+
+exports.searchMovies = (req, res) => {
+  const { query } = req.body;
+  moviedb.search({ query }, (err, res) => {
+    console.log(mdbres);
+    res.send(mdbres);
+  });
+};
---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+  console.log('document ready');
+  // $('#quicksearch').on('submit', (e) => {
+  //   console.log('e.target.value');
+  // });
+});
