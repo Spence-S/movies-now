af323d7b200561ce08ca43d7d5698e1f3d8ca00a
Spencer Snyder <sasnyde2@gmail.com>
2017-09-16 13:51:27 -0400

    added search page

--- controllers/movies.js
+++ controllers/movies.js
@@ -35,9 +35,37 @@ exports.getMovies = (req, res) => {
 };
 
 exports.searchMovies = (req, res) => {
-  const { query } = req.body;
-  moviedb.search({ query }, (err, res) => {
-    console.log(mdbres);
-    res.send(mdbres);
+  const { query } = req.query;
+  if (!query) return exports.getMovies(req, res);
+  let previousUrl;
+  let nextUrl;
+  let { page } = req.query;
+  if (typeof page === 'string') {
+    page = parseInt(page, 10);
+  }
+  if (!page || isNaN(page) || typeof page === 'undefined' || page <= 1) {
+    page = 1;
+    previousUrl = '/movies';
+    nextUrl = `/movies/?page=${page + 1}`;
+  } else {
+    previousUrl = `/movies/?page=${page - 1}`;
+    nextUrl = `/movies/?page=${page + 1}`;
+  }
+
+  moviedb.searchMovie({ query, page }, (err, mdbres) => {
+    const totalPages = mdbres.total_pages;
+    const pagesArray = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pagesArray.push(i);
+    }
+    const options = {
+      mdb: mdbres,
+      page,
+      previousUrl,
+      nextUrl,
+      totalPages,
+      pagesArray,
+    };
+    res.render('movies/search', options);
   });
 };
